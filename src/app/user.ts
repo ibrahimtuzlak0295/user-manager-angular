62967f804d737726c34d8842a0c5fee7df17d221
Ibrahim Tuzlak <ibrahimtuzlak.0295@gmail.com>
2020-07-11 18:17:59 +0200

    User model (interface)

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id?: string,
+    username: string,
+    password: string,
+    first_name: string,
+    last_name: string,
+    address: string,
+    zip: number,
+    city: string
+}
